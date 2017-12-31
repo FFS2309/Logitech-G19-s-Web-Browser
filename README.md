0754e41f6fca0de36644ec6f3bb3daae6ec7fc6c
Fabian Felix Selbach <ffs2309@impulsecraft.de>
2017-12-31 19:59:58 +0100

    Added Page choosing Dialog; Scrolling; Zooming and correct Quit

--- README.md
+++ README.md
@@ -5,10 +5,20 @@ This Applet is designed to bring a Web Browser to your (still) beloved G19/G19s.
 
 # Features
 - Can display google
-
-# Todo
 - Display anything other than google
 - Let the user choose what page to display
+- Zooming (kinda, cause G19 keys are weird sometimes)
+- Scrolling
+
+# Todo
+- Forward Keyboard inputs
+- Forward Mouse inputs
+- Let user navigate to different than startup page
+
+# How to use
+- Scrolling (UP, DOWN, LEFT, RIGHT)
+- Quit (BACK/CANCEL key)
+- Zooming (Hold MENU & press UP(Zoom in)/DOWN(Zoom out)/OK(Reset zoom))
 
 # Requirements
 - lcdjni (https://github.com/djpowell/lcdjni)
--- src/de/impulsecraft/g19webview/Main.java
+++ src/de/impulsecraft/g19webview/Main.java
@@ -12,15 +12,28 @@ import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.imageio.ImageIO;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.text.StyledDocument;
 
 import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.BrowserContext;
+import com.teamdev.jxbrowser.chromium.BrowserContextParams;
 import com.teamdev.jxbrowser.chromium.BrowserType;
 import com.teamdev.jxbrowser.chromium.Callback;
+import com.teamdev.jxbrowser.chromium.JSValue;
+import com.teamdev.jxbrowser.chromium.StorageType;
+import com.teamdev.jxbrowser.chromium.events.FailLoadingEvent;
+import com.teamdev.jxbrowser.chromium.events.FinishLoadingEvent;
+import com.teamdev.jxbrowser.chromium.events.FrameLoadEvent;
+import com.teamdev.jxbrowser.chromium.events.LoadEvent;
+import com.teamdev.jxbrowser.chromium.events.LoadListener;
+import com.teamdev.jxbrowser.chromium.events.ProvisionalLoadingEvent;
+import com.teamdev.jxbrowser.chromium.events.StartLoadingEvent;
 import com.teamdev.jxbrowser.chromium.events.StatusEvent;
 import com.teamdev.jxbrowser.chromium.events.StatusListener;
 import com.teamdev.jxbrowser.chromium.swing.internal.LightWeightWidget;
@@ -38,100 +51,150 @@ import net.djpowell.lcdjni.SyncType;
 
 public class Main {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		ConfigCallback configCallback = new ConfigCallback() {
-			
+
 			@Override
 			public void onConfig() {
 				// TODO Auto-generated method stub
-				
+
 			}
 		};
-		
-		LcdConnection con = new LcdConnection("WebView", false, AppletCapability.getCaps(AppletCapability.QVGA), configCallback, null);
-		try{
+
+		LcdConnection con = new LcdConnection("WebView", false,
+				AppletCapability.getCaps(AppletCapability.QVGA),
+				configCallback, null);
+		try {
 			final AtomicBoolean exit = new AtomicBoolean(false);
 			// #1 Create Browser instance
-	        final Browser browser = new Browser(BrowserType.LIGHTWEIGHT);
-	        BrowserView view = new BrowserView(browser);
+			BrowserContextParams params = new BrowserContextParams("user-data-dir");
+	        params.setStorageType(StorageType.MEMORY);
 
-	        // #2 Set the required view size
-	        browser.setSize(1920, 1440);
+	        BrowserContext browserContext = new BrowserContext(params);
+			final Browser browser = new Browser(BrowserType.LIGHTWEIGHT, browserContext);
+			BrowserView view = new BrowserView(browser);
 
-	        // Wait until Chromium resizes view
-	        Thread.sleep(500);
+			// #2 Set the required view size
+			browser.setSize(320, 240);
 
-	        // #3 Load web page and wait until web page is loaded completely
-	        Browser.invokeAndWaitFinishLoadingMainFrame(browser, new Callback<Browser>() {
-	            @Override
-	            public void invoke(Browser browser) {
-	                browser.loadURL("https://google.com");
-	            }
-	        });
+			// Wait until Chromium resizes view
+			Thread.sleep(500);
 
-	        // Wait until Chromium renders web page content
-	        Thread.sleep(500);
+			// #3 Load web page and wait until web page is loaded completely
+			Browser.invokeAndWaitFinishLoadingMainFrame(browser,
+					new Callback<Browser>() {
+						@Override
+						public void invoke(Browser browser) {
+							browser.loadURL(JOptionPane.showInputDialog("What page do you want to load ?"));
+						}
+					});
 
-	        // #4 Get java.awt.Image of the loaded web page.
-	        LightWeightWidget lightWeightWidget = (LightWeightWidget) view.getComponent(0);
-	        Image image = lightWeightWidget.getImage();
+			// Wait until Chromium renders web page content and scroll to the top left corner
+			Thread.sleep(500);
+			browser.executeJavaScript("window.scrollTo(0, " + "0);");
+			JSValue documentHeight = browser
+					.executeJavaScriptAndReturnValue("Math.max(document.body.scrollHeight, "
+							+ "document.documentElement.scrollHeight);");
+			JSValue documentWidth = browser
+					.executeJavaScriptAndReturnValue("Math.max(document.body.scrollWidth, "
+							+ "document.documentElement.scrollWidth);");
+			final int maxX = (int) documentWidth.getNumberValue(), maxY = (int) documentHeight
+					.getNumberValue();
+
+			// #4 Get java.awt.Image of the loaded web page.
+			LightWeightWidget lightWeightWidget = (LightWeightWidget) view
+					.getComponent(0);
+			Image image = lightWeightWidget.getImage();
 
 			KeyCallback keyCallback = new KeyCallback() {
-				
+				boolean okDown = false;
+				boolean menuDown = false;
 				@Override
 				public void onKeyUp(int button) {
-					// TODO Auto-generated method stub
-					if (button == CANCEL) exit.set(true);
+					if (button == CANCEL && !menuDown)
+						exit.set(true);
+					if (button == OK)
+						okDown = false;
+					if (button == MENU)
+						menuDown = false;
 				}
-				
+
 				@Override
-				public void onKeyDown(int arg0) {
-					// TODO Auto-generated method stub
-					
+				public void onKeyDown(int button) {
+					if (button == OK)
+						okDown = true;
+					if (button == MENU)
+						menuDown = true;
 				}
-				
+
+				int scrollx = 0, scrolly = 0;
+
 				@Override
-				public void onKey(int arg0) {
-					// TODO Auto-generated method stub
-					
+				public void onKey(int button) {
+					if (!menuDown) {
+						if (button == UP) {
+							if (scrolly - 120 > 0) {
+								scrolly -= 120;
+							} else {
+								scrolly = 0;
+							}
+						}
+						if (button == DOWN) {
+							if (scrolly + 120 < maxY) {
+								scrolly += 120;
+							} else {
+								scrolly = maxY - 240;
+							}
+						}
+
+						if (button == LEFT) {
+							scrollx -= (scrollx - 160 > 0) ? 160 : scrollx;
+						}
+						if (button == RIGHT) {
+							scrollx += (scrollx + 160 < maxX) ? 160 : (maxX
+									- scrollx - 240);
+						}
+						if (button == UP || button == DOWN || button == LEFT
+								|| button == RIGHT)
+							browser.executeJavaScript("window.scrollTo("
+									+ scrollx + ", " + scrolly + ");");
+					} else {
+						if(button == UP){
+							browser.zoomIn();
+						}
+						if(button == DOWN){
+							browser.zoomOut();
+						}
+						if(button == OK){
+							browser.zoomReset();
+						}
+					}
 				}
 			};
 			LcdDevice device = con.openDevice(DeviceType.QVGA, keyCallback);
-			try{
-				
-//				JTextPane tp = new JTextPane();
-//				tp.setSize(320, 240);
-//				tp.setContentType("text/html");
-//				Scanner sc = new Scanner(new URL("https://google.com").openStream(), "UTF-8");
-//				sc.useDelimiter("\\A");
-//				String html = sc.next();
-//				sc.close();
-//				tp.setText(html);
-				//tp.setText("<!DOCTYPE html><html><head><title> hi </title></head><body><h1>This is a <b>HEADING</b></h1><p>And this is a paragraph<br>with a new line</p></body></html>");
+			try {
+
 				
 				device.setForeground(true);
 				LcdRGBABitmap bmp = device.createRGBABitmap();
 				bmp.updateScreen(Priority.ALERT, SyncType.SYNC);
-				while(!exit.get()){
+				while (!exit.get()) {
 					Graphics g = bmp.getGraphics();
-//					tp.paint(g);
-					g.drawImage(image, 0, 0, 320, 240, null);
+					g.drawImage(lightWeightWidget.getImage(), 0, 0, 320, 240,
+							null);
 					g.dispose();
 					bmp.updateScreen(Priority.NORMAL, SyncType.SYNC);
 				}
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-			}finally{
+			} finally {
 				device.close();
+				browser.dispose();
 			}
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally{
+		} finally {
 			con.close();
+			System.exit(0);
 		}
-		
+
 	}
-	
 }
